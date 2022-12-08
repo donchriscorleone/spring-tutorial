9f3daa48976f9476a8f64b3e5f7267ef8d225509
Christopher II Lajom <christopheriilajom@gmail.com>
2022-12-08 15:59:01 +0800

    Initialized.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,40 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@Bean
+	public RestTemplate template(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate template) throws Exception {
+		int randomNumberBetween100 = (int)(Math.random() * 100);
+		return args -> {
+			String api = "https://jsonplaceholder.typicode.com/posts/" + randomNumberBetween100;
+			Post post = template.getForObject(api, Post.class);
+			log.info(post.toString());
+		};
 	}
 }
